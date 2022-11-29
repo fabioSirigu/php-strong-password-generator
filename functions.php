a9b6068e3ad471de00a40b0a4dc67714b909fa40
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-29 17:56:08 +0100

    add bonus 1

--- functions.php
+++ functions.php
@@ -1,14 +1,42 @@
 <?php
 
-function generatePassword($length)
+function generatePassword($length, $chars, $numbers, $symbol, $repeat)
 {
       var_dump($length);
       if ($length < 1 || is_null($length) == true) {
             return null;
       } else {
-            $chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
-            $symbol = '-_.:,;§°ç{})(/&$£^+[]';
-            $number = '1234567890';
-            return substr(str_shuffle($chars . $symbol . $number), 0, $length);
+            if (isset($chars)) {
+                  $charsBox = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+            } else {
+                  $charsBox = '';
+            };
+
+            if (isset($numbers)) {
+                  $numbersBox = '0123456789';
+            } else {
+                  $numbersBox = '';
+            };
+
+            if (isset($symbol)) {
+                  $symbolBox = '-_.:,;§°ç{})(/&$£^+[]';
+            } else {
+                  $symbolBox = '';
+            };
+
+            $password = $charsBox . $symbolBox . $numbersBox;
+
+            if ($repeat == "yes") {
+
+                  $reapeatChars = "";
+
+                  for ($i = 0; $i < $length; $i++) {
+                        $reapeatChars .= $password[rand(0, strlen($password) - 1)];
+                  }
+
+                  return $reapeatChars;
+            } else {
+                  return substr(str_shuffle($password), 0, $length);
+            };
       }
 }
--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 include __DIR__ . "/functions.php";
 
-$password = generatePassword($_GET["pswLength"]);
+
+$password = generatePassword($_GET['pswLength'], $_GET['chars'], $_GET['numbers'], $_GET['symbol'], $_GET['repeat']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +14,8 @@ $password = generatePassword($_GET["pswLength"]);
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- BOOTSTRAP -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+      <!-- CSS -->
+      <link rel="stylesheet" href="./assets/css/style.css">
       <title>Document</title>
 </head>
 
@@ -24,9 +27,41 @@ $password = generatePassword($_GET["pswLength"]);
       <main>
             <div class="container text-center my-4">
                   <form action="index.php">
-                        <div>
+                        <div class="lengthPsw">
                               <label for="pswLength">Insersci la lunghezza della password desiderata</label>
-                              <input type="text" name="pswLength" id="pswLength">
+                              <input type="text" name="pswLength" id="pswLength" class="form-control mx-2">
+                        </div>
+                        <div class="mb-3 d-flex justify-content-between px-5">
+                              <h4>Consenti ripetizioni di uno o più caratteri:</h4>
+                              <div>
+
+                                    <input class="form-check-input" type="radio" name="repeat" value="yes" id="repeat" <?php if (isset($_GET['repeat']) && $_GET['repeat'] == 'yes') : ?>checked='checked' <?php endif; ?>>
+                                    <label class="form-check-label" for="yrepeat">
+                                          Sì
+                                    </label>
+
+                                    <input class="form-check-input" type="radio" name="repeat" value="no" id="repeat" <?php if (isset($_GET['repeat']) && $_GET['repeat'] == 'no') : ?>checked='checked' <?php endif; ?>>
+                                    <label class="form-check-label" for="nrepeat">
+                                          No
+                                    </label>
+                              </div>
+
+                        </div>
+                        <div class="px-5 py-3">
+                              <div class="checkbox mt-3">
+                                    <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="chars" id="chars">
+                                          <label class="form-check-label" for="chars">Lettere</label>
+                                    </div>
+                                    <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="numbers" id="numbers">
+                                          <label class="form-check-label" for="numbers">Numeri</label>
+                                    </div>
+                                    <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="symbol" id="symbol">
+                                          <label class="form-check-label" for="symbol">Simboli</label>
+                                    </div>
+                              </div>
                         </div>
                         <button type="submit" class="btn btn-primary">Genera</button>
                   </form>
@@ -39,6 +74,7 @@ $password = generatePassword($_GET["pswLength"]);
                         <h3><strong><?= $password ?></strong></h3>
                   <?php endif ?>
             </div>
+
       </main>
       <footer></footer>
 </body>
